e99634a6e0ac1e0c1662bc5093a6decab28e4350
Asmund Berge <powermosfet@users.noreply.github.com>
2018-05-24 19:54:50 +0200

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,8 +1,11 @@
-onmessage = function(message) {
+onmessage = function(msg) {
   var x = Number(new Date()) + 10000;
+  
+  console.log("Worker received message with count " + msg);
   
   while(x > new Date()) {
   }
   
+  console.log("Worker is done! posting message to main thread");
   postMessage("Done!");
 }

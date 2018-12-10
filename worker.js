037641f7f51fa133406ba483359761cd9b9e18da
Asmund Berge <powermosfet@users.noreply.github.com>
2018-12-10 06:44:57 +0100

    check if worker should keep running

--- worker.js
+++ worker.js
@@ -3,7 +3,7 @@ onmessage = function(msg) {
   
   console.log("Worker received message #" + msg.data);
   
-  while(x > new Date()) {
+  while(x > new Date() && msg.data.keepRunning()) {
   }
   
   console.log("Worker is done processing message #" + msg.data + "! posting message to main thread");
